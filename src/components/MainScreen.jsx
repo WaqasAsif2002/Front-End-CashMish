a9785ac1697d37dca67dd36a76e9a4b3d4939e18
Mohammad Waqas <117596916+WaqasAsif2002@users.noreply.github.com>
2026-01-27 15:20:55 +0500

    finally all good

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -8,7 +8,6 @@ const  PhoneFlipLanding= () =>  {
 <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 overflow-x-hidden">
       {/* Header */}
       <Header />
-      <h1>\waqashere</h1>
 
       {/* Main Content */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:py-6 overflow-hidden">
